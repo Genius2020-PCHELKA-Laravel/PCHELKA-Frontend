2c3292736f68c68e1a7923b1c6d7f25b70dbc6a3
Ali-Rahal <aliyrahal1995@gmail.com>
2020-05-15 15:07:22 +0300

    haladone

--- src/screens/Frequency.js
+++ src/screens/Frequency.js
@@ -1,15 +1,50 @@
-import React ,{useEffect}from 'react';
+import React ,{useState}from 'react';
 import {  StyleSheet,  View, TouchableOpacity } from 'react-native';
-import {Container,Footer, FooterTab,Button,Text,} from 'native-base';
+import {Container,Footer, FooterTab,Button,} from 'native-base';
 import { CheckBox,Icon } from 'react-native-elements'
-import Radio1 from './Radio1';
+import { RadioButton, Text } from 'react-native-paper';
 
 const Frequency = ({ navigation }) => {
- 
+ const [state,setState]=useState(0);
   return (
   <View style={{flex:1}}> 
     
-    <Radio1  />
+    <RadioButton.Group
+        onValueChange={setState}
+          
+        
+        value={state}
+
+    > 
+
+        <View style ={{marginBottom: 30}}>
+  <Text>{state}</Text>      
+        <View style={{flexDirection:'row'}}>
+        <RadioButton value="1" />
+          <Text style={{fontSize:23}}>One-time</Text>
+
+       
+          </View>
+          <Text style={{marginLeft:35}}>Book acleaning for one time only</Text>
+        </View>
+        <View>
+        <View style={{flexDirection:'row'}}>
+        <RadioButton value="2" />
+          <Text  style={{fontSize:23}}>Bi-weekly</Text>
+          
+          </View>
+          <Text style={{marginLeft:35}}>Book a recurring cleaning with the same cleaner every two-weeks</Text>
+        </View>
+        <View>
+        <View style={{flexDirection:'row'}}>
+        <RadioButton value="3" />
+          <Text  style={{fontSize:23}}>Weekly</Text>
+          
+          </View>
+          <Text style={{marginLeft:35}}>Book a recurring with the same cleaner every week</Text>
+        </View>
+      </RadioButton.Group>
+    
   
   <Footer>
           <FooterTab>
--- src/screens/Payment.js
+++ src/screens/Payment.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
-import { CheckBox } from 'react-native-elements'
-
+import React ,{useState}from 'react';
+import { Text, StyleSheet, View,  TouchableOpacity } from 'react-native';
+import { RadioButton} from 'react-native-paper';
+import {Container,Footer, FooterTab,Button,} from 'native-base';
 
 const Payment= ({ navigation }) => {
-    
+  const [state,setState]=useState('1');
   
   return (
   <View> 
     <Text style={styles.text}>Payment screen </Text>
     
  
+    <RadioButton.Group
+        onValueChange={setState}
+          
+        value={state}
+        
+
+    > 
+
+        <View style ={{marginBottom: 30}}>
+      
+        <View style={{flexDirection:'row'}}>
+        <RadioButton value="1" />
+  <Text style={{fontSize:23}}>Pay by credit/debit card </Text>
+
+       
+          </View>
+          <Text style={{marginLeft:35}}>insuurance when you pay online AED 1000</Text>
+        </View>
+        <View>
+        <View style={{flexDirection:'row'}}>
+        <RadioButton value="2" />
+          <Text  style={{fontSize:23}}>Pay with cash (+5 AED)</Text>
+          
+          </View>
+         </View>
+        
+      </RadioButton.Group>
   </View>);
 };
 
