ada91c30e01e0eec6b1ff03729caf6c8f873c4ec
MaherSalama2020 <dr.maher.salama@gmail.com>
2020-05-26 18:28:45 -0700

    HH##Comment unused files #1

--- src/screens/DateAndTime.js
+++ src/screens/DateAndTime.js
@@ -1,75 +1,75 @@
-import React ,{useState,useEffect}from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+// import React ,{useState,useEffect}from 'react';
+// import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-import {Badge} from 'native-base';
-import {Container,Footer, FooterTab, Card, CardItem,Body} from 'native-base';
+// import {Badge} from 'native-base';
+// import {Container,Footer, FooterTab, Card, CardItem,Body} from 'native-base';
 
-const DateAndTime= ({ navigation }) => {
-  const[token,settoken]=useState('1');
+// const DateAndTime= ({ navigation }) => {
+//   const[token,settoken]=useState('1');
 
-  getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem('token')
-      console.log(value);
-      if(value !='') {
-        settoken(value)
-      }
-    } catch(e) {
-      // error reading value
-    }
-  }
-/*
-  useEffect(() => {
-    this.getData();
+//   getData = async () => {
+//     try {
+//       const value = await AsyncStorage.getItem('token')
+//       console.log(value);
+//       if(value !='') {
+//         settoken(value)
+//       }
+//     } catch(e) {
+//       // error reading value
+//     }
+//   }
+// /*
+//   useEffect(() => {
+//     this.getData();
 
-  },[]);*/
-  return (
-  <View> 
-    <Text style={styles.text}>DateAndTime screen </Text>
-    
-    <Text style={styles.text}>When would you like your cleaning? </Text>
-    <View style={{flexDirection:'row'}}><Badge info style={{fontSize:20,marginStart:30}}>
-       <Text  style={{fontSize:20}}>6</Text> 
-       </Badge>
-       <Badge info style={{fontSize:20,marginStart:30}}>
-       <Text  style={{fontSize:20}}>7</Text> 
-       </Badge>
-    </View>
-    
-    <Text style={styles.text}>What time would you like us to start? </Text>
-    <View style={{flexDirection:'row'}}><Badge info style={{fontSize:20,marginStart:30}}>
-       <Text  style={{fontSize:20}}>13:00</Text> 
-       </Badge>
-       <Badge info style={{fontSize:20,marginStart:30}}>
-       <Text  style={{fontSize:20}}>13:30</Text> 
-       </Badge>
-    </View>
-    <Footer>
-    <FooterTab>
-    <Text>Total $:</Text>
-          
-         <TouchableOpacity onPress={()=>{token? navigation.navigate('Addresses'):navigation.navigate('loginph')}}> 
-         <Badge info style={{fontSize:20,marginStart:30}}>
-       <Text  style={{fontSize:20}}>Next</Text> 
-       </Badge>
-            </TouchableOpacity>
-         
-            
-          </FooterTab>
-        </Footer>
-  
-    
-    
-   
-      
-        
-  </View>);
-};
+//   },[]);*/
+//   return (
+//   <View> 
+//     <Text style={styles.text}>DateAndTime screen </Text>
 
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 25
-  }
-});
+//     <Text style={styles.text}>When would you like your cleaning? </Text>
+//     <View style={{flexDirection:'row'}}><Badge info style={{fontSize:20,marginStart:30}}>
+//        <Text  style={{fontSize:20}}>6</Text> 
+//        </Badge>
+//        <Badge info style={{fontSize:20,marginStart:30}}>
+//        <Text  style={{fontSize:20}}>7</Text> 
+//        </Badge>
+//     </View>
 
-export default DateAndTime;
+//     <Text style={styles.text}>What time would you like us to start? </Text>
+//     <View style={{flexDirection:'row'}}><Badge info style={{fontSize:20,marginStart:30}}>
+//        <Text  style={{fontSize:20}}>13:00</Text> 
+//        </Badge>
+//        <Badge info style={{fontSize:20,marginStart:30}}>
+//        <Text  style={{fontSize:20}}>13:30</Text> 
+//        </Badge>
+//     </View>
+//     <Footer>
+//     <FooterTab>
+//     <Text>Total $:</Text>
+
+//          <TouchableOpacity onPress={()=>{token? navigation.navigate('Addresses'):navigation.navigate('loginph')}}> 
+//          <Badge info style={{fontSize:20,marginStart:30}}>
+//        <Text  style={{fontSize:20}}>Next</Text> 
+//        </Badge>
+//             </TouchableOpacity>
+
+
+//           </FooterTab>
+//         </Footer>
+
+
+
+
+
+
+//   </View>);
+// };
+
+// const styles = StyleSheet.create({
+//   text: {
+//     fontSize: 25
+//   }
+// });
+
+// export default DateAndTime;
--- src/screens/freecleaning.js
+++ src/screens/freecleaning.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button,SafeAreaView, TouchableOpacity } from 'react-native';
+// import React from 'react';
+// import { Text, StyleSheet, View, Button,SafeAreaView, TouchableOpacity } from 'react-native';
 
-import {FontAwesome5} from '@expo/vector-icons';
+// import {FontAwesome5} from '@expo/vector-icons';
 
-const freecleaning = ({ navigation }) => {
-    
-  
-  return (
-    <View style={styles.container}>
-    <SafeAreaView style={{flex:1}}>
-      <TouchableOpacity style={{alignItems:"flex-end",margin:16,paddingTop:20}} 
-       onPress={navigation.openDrawer }>
-        <FontAwesome5 name="bars" size={24} color="#161924" />
-      </TouchableOpacity>
-      <View>
-         <Text style={styles.Text}> free cleacning screen</Text>
-      </View>
-    </SafeAreaView>
-  </View>);
-};
+// const freecleaning = ({ navigation }) => {
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  Text : {
-     color :"#161924",
-     fontSize :20,
-     fontWeight:"500"
-  }
-});
+//   return (
+//     <View style={styles.container}>
+//     <SafeAreaView style={{flex:1}}>
+//       <TouchableOpacity style={{alignItems:"flex-end",margin:16,paddingTop:20}} 
+//        onPress={navigation.openDrawer }>
+//         <FontAwesome5 name="bars" size={24} color="#161924" />
+//       </TouchableOpacity>
+//       <View>
+//          <Text style={styles.Text}> free cleacning screen</Text>
+//       </View>
+//     </SafeAreaView>
+//   </View>);
+// };
 
-export default freecleaning;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+//   Text : {
+//      color :"#161924",
+//      fontSize :20,
+//      fontWeight:"500"
+//   }
+// });
+
+// export default freecleaning;
