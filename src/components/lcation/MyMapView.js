f4223a22d0259a76c07e902d5eed612a0d187147
MaherSalama2020 <dr.maher.salama@gmail.com>
2020-06-14 05:31:37 -0700

    maps

--- src/components/lcation/MyMapView.js
+++ src/components/lcation/MyMapView.js
@@ -5,7 +5,7 @@
 //     return (
 //         <MapView
 //             style={{ flex: 1, position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
-//             region={{ latitude: props.latitude, longitude: props.longitude, latitudeDelta: props.latitudeDelta, longitudeDelta: props.longitudeDelta }}
+//             region={{ latitude: parseFloat(props.latitude), longitude: parseFloat(props.longitude), latitudeDelta: parseFloat(props.latitudeDelta), longitudeDelta: parseFloat(props.longitudeDelta) }}
 //             showsUserLocation={true}
 //             loadingEnabled
 //             maxDelta={0.003}
@@ -17,7 +17,7 @@
 //             }}
 //         >
 //             <Marker
-//                 coordinate={{ latitude: props.latitude, longitude: props.longitude, latitudeDelta: props.latitudeDelta, longitudeDelta: props.longitudeDelta }}
+//                 coordinate={{ latitude: parseFloat(props.latitude), longitude: parseFloat(props.longitude), latitudeDelta: parseFloat(props.latitudeDelta), longitudeDelta: parseFloat(props.longitudeDelta) }}
 //                 pinColor="#d21404" />
 //         </MapView>
 //     )
